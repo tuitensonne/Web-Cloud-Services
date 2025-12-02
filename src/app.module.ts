7a4c1cc445b1abfa94ddadc6634f9f47eda59223
sontapcode <156274946+tuitensonne@users.noreply.github.com>
2025-12-02 10:40:06 +0700

    Update app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -10,7 +10,7 @@ import { AppService } from './app.service';
     UsersModule,
     TypeOrmModule.forRoot({
       type: 'postgres',
-      url: process.env.DATABASE_URL,
+      url: "postgresql://my_nestjs_app_user:ngDhkbcTQSTxR4IbFLvJckvc3uaUiUZj@dpg-d4n577ili9vc73fbevmg-a.singapore-postgres.render.com/my_nestjs_app?sslmode=require",
       entities: [User],
       synchronize: true, 
       ssl: { rejectUnauthorized: false }, 
